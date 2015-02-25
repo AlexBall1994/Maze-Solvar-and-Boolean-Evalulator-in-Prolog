9dd06f5c2d4a4312a98a29511ea65d7e213d3ee1
alexball1994 <aball9423@comcast.net>
2015-02-24 19:41:26 -0500

    first commit

---
+++ goTest.pl
@@ -0,0 +1,25 @@
+:- initialization(
+    [ 'logic.pl'
+    , 'publicRecursion1.pl'
+    , 'publicRecursion2.pl'
+    , 'publicMaze1.pl'
+    , 'publicMaze2.pl'
+    ]).
+run_f(Func) :-
+    % name(Func,FunName),
+    % writef("Testcase: %s\n",[FunName]),
+    Func.
+testcases(
+    [ public_prod
+    , public_fill
+    , public_genN
+    , public_genXY
+    , public_flat
+    , public_stats
+    , public_validPath
+    , public_findDistance
+    , public_solve
+    ]).
+run :-
+    testcases(TC),
+    maplist(run_f,TC).
